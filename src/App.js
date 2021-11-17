158c7582eca42373db6f8e6ea3118c0f2d5652eb
jurajrc <81872592+jurajrc@users.noreply.github.com>
2021-11-17 19:50:58 +0100

    2. modifi content

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import { animeResult } from './animations';
 function App() {
   // useState
   const [weather, setWeather] = useState(null)
-  const [weather2, setWeather2] = useState(null)
+  //const [weather2, setWeather2] = useState(null)
 
   const [input, setInput] = useState("")
   const [input2, setInput2] = useState("")
@@ -50,7 +50,8 @@ const searchWeather = async (e) => {
     await axios.get(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API}&q=${input2}&aqi=yes&days=5&lang=sk`)
     .then(data => {
       setWeather(data.data);
-    });
+    })
+    .catch(err => console.log(err))
     setTimeout(() => {
     }, 100);
     setInput("")
@@ -68,7 +69,8 @@ const searchWeather = async (e) => {
       setToday(data.data.forecast.forecastday[0])
       setTomorrow(data.data.forecast.forecastday[1])
       setAftertomorrow(data.data.forecast.forecastday[2])
-    });
+    })
+    .catch(err => console.log(err))
     setTimeout(() => {
     }, 100);
     setInput2("")
@@ -92,35 +94,59 @@ const fixedNumber = km => ((km * 1000) / 60 / 60).toFixed(1)
       <Header />
         <StyleFullView variants={animeResult} initial="hidden" animate="show" exit="exit">
           <div className="content">
-            <form className="search">
+            {/* <form className="search">
               <input onChange={weatherImput} value={input} type="text" />
               <button onClick={searchWeather} >Hladať</button>
-            </form>
+            </form> */}
             <form className="search2">
               <input onChange={weatherImput2} value={input2} type="text" />
-              <button onClick={searchWeather2} >Hladať</button>
+              <button onClick={ searchWeather, searchWeather2 } >Hladať</button>
             </form>
             { today && (
-              <StyleResult>
+              <ActualResult>
                 <StyleLocation>
-                <h3>{location.country}, </h3>
-                <h3>{location.name}</h3>
+                  <h3>{location.country}, </h3>
+                  <h3>{location.name}</h3>
                 </StyleLocation>
-                <OneLine>
-                <img src={today.day.condition.icon} alt="icon" />
-                <h2>{today.day.condition.text}</h2>
-                </OneLine>
-                <OneLine>
-                  <h1>{weather.current.temp_c} &#8451;</h1>
-                  <h3>Min {today.day.mintemp_c} &#8451;</h3>
-                  <h3>Max {today.day.maxtemp_c} &#8451;</h3>
+                <StyleBlocks>
+                  <StyleBlockWrap>
+                    <div className="block">
+                      <h1>{weather.current.temp_c}&#8451;</h1>
+                    </div>
+                    <div className="block oneline">
+                      <img src={today.day.condition.icon} width="30px" alt="icon" />
+                      <p>{today.day.condition.text}</p>
+                    </div>
+                    <div className="block under">
+                      <p>Max</p>
+                      <p>{today.day.maxtemp_c} &#8451;</p>
+                    </div>
+                    <div className="block under">
+                      <p>Max</p>
+                      <p>{today.day.mintemp_c} &#8451;</p>
+                    </div>
+                    <div className="block under">
+                      <p>Východ slnka</p>
+                      <p>{today.astro.sunrise}</p>
+                    </div>
+                    <div className="block under">
+                      <p>Vlhkosť</p>
+                      <p>{today.day.avghumidity}%</p>
+                    </div>
+                    <div className="block under">
+                      <p>Viditelnosť</p>
+                      <p>{weather.current.vis_km}km</p>
+                    </div>
+                    <div className="block under">
+                      <p>Vietor</p>
+                      <p>{ fixedNumber(weather.current.wind_kph) }m/s {weather.current.wind_dir}</p>
+                    </div>
 
-                </OneLine>
-                <h3>Vyditelnosť {weather.current.vis_km}km</h3>
-                <h3>Vietor { fixedNumber(weather.current.wind_kph) }m/s {weather.current.wind_dir}</h3>
-                <p>Posledný údaj {weather.current.last_updated} </p>
-              </StyleResult>
+                  </StyleBlockWrap>
+                </StyleBlocks>
+              </ActualResult>
             )}
+            
           </div>
         </StyleFullView>
     </StyleApp>
@@ -142,7 +168,7 @@ const StyleFullView = styled(motion.div)`
 
   .content {
     padding: 0 15%;
-    @media (max-width: 500px) {
+    @media (max-width: 770px) {
       padding: 0 10%;
     }
   }
@@ -150,6 +176,9 @@ const StyleFullView = styled(motion.div)`
   form {
     padding: 2em 0;
     position: relative;
+    @media (max-width: 550px) {
+      padding: 1em 0;
+    }
 
     input {
       width: 100%;
@@ -173,12 +202,22 @@ const StyleFullView = styled(motion.div)`
       top: 36.1%;
       cursor: pointer;
       transition: .3s all ease ;
+      @media (max-width: 550px) {
+      top: 29.2%;
+    }
       &:hover {
         //background: darken(0.5, rgb(58, 144, 255));
         background: rgb(0, 110, 255);
       }
     }
   }
+`
+const ActualResult = styled(motion.article)`
+  display: flex;
+  flex-direction: column;
+  color: #e7e7e7;
+  border-radius: 8px;
+  overflow: hidden;
 `
 const StyleLocation = styled.div`
   background: #eeeeee7d;
@@ -189,6 +228,41 @@ const StyleLocation = styled.div`
     margin: 0.2em;
     font-weight: 600;
   }
+`
+const StyleBlocks = styled.div`
+  display: flex;
+  background: #0000007b;;
+`
+const StyleBlockWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  .block {
+    width: 25%;
+    min-height: 6em;
+    @media (max-width: 650px) {
+      width: 50%;
+    }
+    h1 {
+      font-size: 2.5em;
+      margin: 0.5em 0;
+    }
+    p {
+      margin: 0;
+      font-size: 0.8em;
+    }
+  }
+  .oneline {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .under {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  
 `
 const StyleResult = styled.article`
   background: #0000007b;
