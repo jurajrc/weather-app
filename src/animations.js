a68d3a00105bded4759d7d536a6555dd3b07a01b
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-02-02 22:49:10 +0100

    12. add object animation.js

--- src/animations.js
+++ src/animations.js
@@ -1,21 +1,59 @@
-export const animeResult = {
+export const animeInput = {
     hidden: {
-        scale: 0.2,
+        x: "1000px",
         opacity: 0,
     },
     show: {
-        scale: 1,
+        x: "0px",
         opacity: 1,
         transition: {
-            duration: 1,
-            delay: 1.5
+            //type: "spring"
+             duration: 0.75,
+             delay: 1,
+            // ease: "backInOut"
+        }
+    }
+}
+export const popup = {
+    hidden: {
+        opacity: 0,
+        scale: 0.2
+    },
+    show: {
+        opacity: 1,
+        scale: 1,
+        transition: {
+            duration: 0.75,
+            ease: 'easeOut'
         }
     },
     exit: {
-        x: "100vh",
         opacity: 0,
-        transition: {
-            duration: 0.5,
-        }
+        scale: 0.2
+    }
+}
+export const showToTop = {
+    hidden: {
+        y: "-4em",
+        opacity: 0,
     },
+    show: {
+        y: "0em",
+        opacity: 1,
+        transition: {
+             duration: 0.75
+        }
+    }
+}
+export const showText = {
+    hidden: {
+        y: "50px"
+    },
+    show: {
+        y: "0px",
+        transition: {
+            duration: 0.75,
+            delay: 0.75
+        }
+    }
 }
