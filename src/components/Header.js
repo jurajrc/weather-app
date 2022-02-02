95cd0ffb7f232dd526d069f5dc84fdb4b7833ddf
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-02-02 22:47:14 +0100

    10. add animate elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,59 @@
 import React from 'react'
 // Stale
 import styled from 'styled-components'
+// Animation
+import { motion } from 'framer-motion';
+import { showToTop, showText } from '../animations';
 // fontAwesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWind } from '@fortawesome/free-solid-svg-icons'
 
 const Header = () => {
     return (
-        <StyleHeader>
-            <div className="left">
-                <FontAwesomeIcon icon={faWind} />
-                <p> 
-                    Aktualne počasie
-                </p>
-            </div>
-            
-            {/* <a href="https://www.weatherapi.com/"> <img src="//cdn.weatherapi.com/v4/images/weatherapi_logo.png" width="60px" alt="Weather data by WeatherAPI.com" /> </a> */}
-            <a href="https://www.weatherapi.com/" target="_blank" rel="noreferrer" >Weather <span>API</span> </a>
+        <StyleHeader variants={showToTop} initial="hidden" animate="show" >
+            <Hide>
+                <motion.div 
+                    className="left"
+                    variants={showText}
+                    >
+                    <FontAwesomeIcon icon={faWind} />
+                    <p> 
+                        Aktuálne počasie
+                    </p>
+                </motion.div>
+            </Hide>
+            <Hide>
+                {/* <a href="https://www.weatherapi.com/"> <img src="//cdn.weatherapi.com/v4/images/weatherapi_logo.png" width="60px" alt="Weather data by WeatherAPI.com" /> </a> */}
+                <motion.a 
+                    className="left"
+                    variants={showText}
+                    href="https://www.weatherapi.com/" target="_blank" rel="noreferrer" >Weather
+                    <span>API</span> 
+                </motion.a>
+            </Hide>
             
         </StyleHeader>
     )
 }
 
-const StyleHeader = styled.header`
-    max-width: 100%;
-    min-height: 6vh;
+const StyleHeader = styled(motion.header)`
+    width: 100%;
+    height: 4em;
+    position: fixed;
+    top: 0;
+    left: 0;
     background: #0000007b;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0 15%;
     color: #fff;
+    z-index: 1;
     @media (max-width: 770px) {
       padding: 0 10%;
+    }
+    @media (max-width: 550px) {
+      height: 3em
     }
     .left {
         display: flex;
@@ -54,6 +75,9 @@ const StyleHeader = styled.header`
             color: rgb(58, 144, 255);
         }
     }
+`
+const Hide = styled.div`
+  overflow: hidden;
 `
 
 export default Header
